d831bd6278e66807c5f18384b7bc69f18895eae9
Pablo Lluch <pablo.lluch@gmail.com>
2014-10-29 18:08:54 -0300

    Modify API url

--- www/js/services/rest.js
+++ www/js/services/rest.js
@@ -2,7 +2,7 @@
 var outlinerServices = angular.module('outlinerServices', ['ngResource']);
 outlinerServices.factory('Section', ['$resource',
 	function($resource) {
-		return $resource('http://127.0.0.1:3000/sections/:id', { id: '@id' }, {
+		return $resource('http://outliner-rails.herokuapp.com/sections/:id', { id: '@id' }, {
 			query: { method: 'GET', params: { }, isArray: true, transformResponse: function(data, headersGetter) {
 				return angular.fromJson(data);
 			}},
