5cddb235e56488a333434dbfbb7898c5973ecc59
Pablo Lluch <pablo.lluch@gmail.com>
2014-11-03 00:13:09 -0300

    Change rest url

--- www/js/services/rest.js
+++ www/js/services/rest.js
@@ -2,7 +2,7 @@
 var outlinerServices = angular.module('outlinerServices', ['ngResource']);
 outlinerServices.factory('Outline', ['$resource',
 	function($resource) {
-		return $resource('http://127.0.0.1:3000/outlines/:id', { id: '@id' }, {
+		return $resource('http://outliner-rails.herokuapp.com/outlines/:id', { id: '@id' }, {
 			query: { method: 'GET', params: { }, isArray: true, transformResponse: function(data, headersGetter) {
 				return angular.fromJson(data);
 			}},
